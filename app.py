598f524a23ffe392271bc6e072f94601a11bcadb
moisefr <38119115+moisefr@users.noreply.github.com>
2025-11-27 12:17:03 -0500

    try4

--- app.py
+++ app.py
@@ -190,6 +190,7 @@ def calculations():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug= True)
+   # port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port, debug= True)
+    app.run()
 
